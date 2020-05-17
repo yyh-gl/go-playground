b5fd3c4d4257cedb8d6dc1b6686f873682e8acdf
yyh-gl <yhonda.95.gl@gmail.com>
2020-05-17 16:49:47 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("========================")
+	fmt.Println("main")
+	fmt.Println("========================")
+}
